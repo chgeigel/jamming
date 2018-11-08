894538214a099e6d64f2999c2e81aee97e99c53a
Chris Geigel <chris.geigel@autozone.com>
2018-11-08 03:27:57 -0600

    fixing login

--- jamming/src/server.js
+++ jamming/src/server.js
@@ -8,13 +8,56 @@
  */
 
 var express = require('express'); // Express web server framework
-const cors = require('cors');
-require('dotenv').config();
+var cors = require('cors');
+var dotenv = require('dotenv').config({path: '../.env'});
 
-var port = process.env.REACT_APP_SPOTIFY_LOGIN_PORT || 8888;
+if ( dotenv.error) {
+  throw dotenv.error;
+}
+var port = process.env.REACT_APP_SPOTIFY_LOGIN_PORT || 5000;
+var client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID; // Your client id
+var redirect_uri = process.env.REACT_APP_SPOTIFY_REDIRECT_URL; // Your redirect uri
+var url = process.env.REACT_APP_SPOTIFY_AUTH_API_URL;
+
 
 var app = express();
 app.use(cors());
 app.options('*',cors());
-app.use(express.static(__dirname + '/server_public'));
+//app.use(express.static(__dirname + '/server_public'));
+
+function generateRandomString(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+app.get('/', function(req,res) {
+  console.log('here');
+  var stateKey = 'spotify_auth_state';
+  /**
+   * Generates a random string containing numbers and letters
+   * @param  {number} length The length of the string
+   * @return {string} The generated string
+   */
+
+
+  var state = generateRandomString(16);
+
+  var scope = 'user-read-private user-read-email playlist-modify-public playlist-modify-private';
+  var parameters = '?response_type=token' +
+                   '&client_id=' + encodeURIComponent(client_id) +
+                   '&scope=' + encodeURIComponent(scope) +
+                   '&redirect_uri=' + encodeURIComponent(redirect_uri) +
+                   '&state=' + encodeURIComponent(state);
+
+  //window.location = url;
+  console.log(url + parameters);
+  res.redirect(url + parameters);
+
+});
+
 app.listen(port, ()=>console.log(`Listening on ${port}`));
